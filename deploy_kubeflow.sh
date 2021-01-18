f2181fea9d13b6dc965e6b9d455eb4f27625529b
Antoine Hébert <hantoine02@gmail.com>
2021-01-17 21:13:46 -0500

    Add spot p3.2xlarge nodes

--- deploy_kubeflow.sh
+++ deploy_kubeflow.sh
@@ -2,14 +2,13 @@ read -p "Enter a name for the cluster: " cluster_name
 read -p "Enter Kubeflow admin e-mail: " admin_email
 read -p "Enter Kubeflow admin password: " admin_password
 
-# Kubeflow assumes the current folder's name is the name of the cluster when configuring the ALB
 mkdir $cluster_name && cd $cluster_name
 
-sed "s/<name>/$cluster_name/" ../m5spot_template.yaml > m5spot.yaml
-eksctl create cluster -f m5spot.yaml
+sed "s/<name>/$cluster_name/" ../cluster_template.yaml > cluster.yaml
+eksctl create cluster -f cluster.yaml
 
-sed "s/<admin_password>/$admin_password/ ; s/<admin_email>/$admin_email/" ../kfctl_aws_template.yaml > kfctl_aws.yaml
-kfctl apply -V -f kfctl_aws.yaml
+sed "s/<cluster_name>/$cluster_name/ ; s/<admin_password>/$admin_password/ ; s/<admin_email>/$admin_email/" ../kfctl_aws_template.yaml > kfctl_aws.yaml
+kfctl apply -f kfctl_aws.yaml
 
 sleep 5
 kubectl get pod -n kubeflow --no-headers | grep -v Running >> /dev/null
